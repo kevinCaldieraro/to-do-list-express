9715b60df5e17c1941e8476228a9b338839d2434
Kevin Caldieraro <kevincaldieraro123@gmail.com>
2022-08-02 00:50:31 -0300

    update app.js

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+const path = require('path');
 const express = require('express');
+const rootRouter = require('./src/routes/index');
 const checklistRouter = require('./src/routes/checklist');
 require('./config/database');
 
-const app = express();
 const PORT = 3000;
-
+const app = express();
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
+app.use('/', rootRouter);
 app.use('/checklists', checklistRouter);
 
 app.listen(PORT, () => {
