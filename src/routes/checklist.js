23f2cfd34b9d9de12204490ed3c64d99c2356798
Kevin Caldieraro <kevincaldieraro123@gmail.com>
2022-08-01 22:54:24 -0300

    include of requests patch and delete

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,19 +1,57 @@
 const express = require('express');
+const Checklist = require('../models/checklist');
 
 const router = express.Router();
 
 router
-  .get('/', (req, res) => {
-    console.log('Hello');
-    res.send();
+  .get('/', async (req, res) => {
+    try {
+      let checklists = await Checklist.find({});
+      res.status(200).json(checklists);
+    } catch (error) {
+      res.status(500).json(error);
+    }
   })
-  .post('/', (req, res) => {
-    console.log(req.body);
-    res.status(200).json(req.body);
+  .post('/', async (req, res) => {
+    try {
+      let { title } = req.body;
+      let checklist = await Checklist.create({ title });
+      res.status(200).json(checklist);
+    } catch (error) {
+      res.status(422).json(error);
+    }
   })
-  .get('/:id', (req, res) => {
-    console.log(req.params.id);
-    res.send(`ID: ${req.params.id}`);
+  .get('/:id', async (req, res) => {
+    try {
+      let ID = req.params.id;
+      let checklist = await Checklist.findById(ID);
+      res.status(200).json(checklist);
+    } catch (error) {
+      res.status(422).json(error);
+    }
+  })
+  .patch('/:id', async (req, res) => {
+    try {
+      let ID = req.params.id;
+      let { title } = req.body;
+      let checklist = await Checklist.findByIdAndUpdate(
+        ID,
+        { title },
+        { new: true }
+      );
+      res.status(200).json(checklist);
+    } catch (error) {
+      res.status(422).json(error);
+    }
+  })
+  .delete('/:id', async (req, res) => {
+    try {
+      let ID = req.params.id;
+      let checklist = await Checklist.findByIdAndRemove(ID);
+      res.status(200).json({ message: `${checklist.title} deleted` });
+    } catch (error) {
+      res.status(422).json(error);
+    }
   });
 
 module.exports = router;
