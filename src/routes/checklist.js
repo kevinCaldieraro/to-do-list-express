378cf91e9284be0e5a00b7ceaafe3f90db706346
Kevin Caldieraro <kevincaldieraro123@gmail.com>
2022-08-04 04:34:33 -0300

    update checklist route

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -7,24 +7,28 @@ router
   .get('/', async (req, res) => {
     try {
       let checklists = await Checklist.find({});
-      res.status(200).json(checklists);
+      res.status(200).render('checklists/index', { checklists: checklists });
     } catch (error) {
-      res.status(500).json(error);
-    }
-  })
-  .post('/', async (req, res) => {
-    try {
-      let { title } = req.body;
-      let checklist = await Checklist.create({ title });
-      res.status(200).json(checklist);
-    } catch (error) {
-      res.status(422).json(error);
+      res
+        .status(500)
+        .render('pages/error', { error: 'Cannot find checklists' });
     }
   })
   .get('/:id', async (req, res) => {
     try {
       let ID = req.params.id;
       let checklist = await Checklist.findById(ID);
+      res
+        .status(200)
+        .render('checklists/show_checklist', { checklist: checklist });
+    } catch (error) {
+      res.status(422).render('pages/error', { error: 'Cannot find checklist' });
+    }
+  })
+  .post('/', async (req, res) => {
+    try {
+      let { title } = req.body;
+      let checklist = await Checklist.create({ title });
       res.status(200).json(checklist);
     } catch (error) {
       res.status(422).json(error);
